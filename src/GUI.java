348066caffe2d015f8ddb66040d67992985de018
Mateo A <mateo@MacBookAir.lan>
2025-09-10 21:49:24 -0700

    commited to repo using autogit

--- src/GUI.java
+++ src/GUI.java
@@ -16,14 +16,37 @@ public class GUI
         JFrame frame = new JFrame("Flashcard101");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        frame.setSize(1200, 700);
+        frame.setSize(1200, 1200);
         frame.setResizable(true);
         frame.setVisible(true);
 
-        JLabel title = new JLabel("Flashcard101", SwingConstants.CENTER);
+        JLabel title = new JLabel("Flashcard101: An Interactive Flashcard Application", SwingConstants.CENTER);
+        title.setFont(new Font("Georgia", Font.PLAIN, 36));
         frame.add(title, BorderLayout.NORTH);
 
-        JPanel textBoxPanel = new JPanel();
+        JLabel credits = new JLabel("Created by Mateo Alado, Amreen Ahmed, Sanad Atia, Ohenewaa Ampem Darko, and Twinkle Johnson - Fall 2025", SwingConstants.CENTER);
+        credits.setFont(new Font("Georgia", Font.PLAIN, 28));
+        frame.add(credits, BorderLayout.SOUTH);
+
+        JPanel reviewPanel = new JPanel();
+        reviewPanel.setLayout(new FlowLayout());
+        frame.add(reviewPanel, BorderLayout.EAST);
+
+        JPanel createPanel = new JPanel();
+        createPanel.setLayout(new FlowLayout());
+        frame.add(createPanel, BorderLayout.WEST);
+
+        JButton reviewButton = new JButton("Review Flashcards");
+        reviewButton.setFont(new Font("Georgia", Font.PLAIN, 24));
+        reviewButton.setPreferredSize(new Dimension(600, 700));
+        reviewPanel.add(reviewButton, BorderLayout.CENTER);
+        
+        JButton createButton = new JButton("Create and Modify");
+        createButton.setFont(new Font("Georgia", Font.PLAIN, 24));
+        createButton.setPreferredSize(new Dimension(600, 700));
+        createPanel.add(createButton, BorderLayout.CENTER);
+
+        /*JPanel textBoxPanel = new JPanel();
         frame.add(textBoxPanel, BorderLayout. CENTER);
 
         JLabel textBoxLabel = new JLabel("Enter a Flashcard Set: ");
@@ -39,7 +62,9 @@ public class GUI
         textBoxPanel.add(inputField);
         textBoxPanel.add(submitButton);
 
-        set = new FlashcardSet(dir + filename);
+        set = new FlashcardSet(dir + filename + ".csv");
+
+        */
     }
 
     public static void main(String[] args)
